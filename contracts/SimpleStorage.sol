247cbb58a4f81bc3143830950bb20719060f1137
Hussain Ali <ali.415code@gmail.com>
2021-09-18 17:08:15 -0700

    Added Simple Storage

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.4.21 <0.7.0;
 
 contract SimpleStorage {
-  constructor() public {
+  uint storedData;
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() public view returns (uint) {
+    return storedData;
   }
 }
